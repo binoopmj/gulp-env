2134e881f90a9df3f21008b798f01d70d2c24c3f
Binoop MJ <binoop.mj@fayausa.com>
2018-05-04 14:23:42 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+Gulp Environment
+---
+
+What's set up here:
+
+- Sass (LibSass)
+- CSS Minification
+- BrowserSync
+- Image Minification
+
+## To Get Started:
+
+1. Make sure npm (Node Package Manager) is installed(https://www.npmjs.com/get-npm)
+2. Clone this repo to build your gulp environment or download the zip
+3. Open treminal and  `cd` (change directory) to the folder containing your project.
+4. Run `rm -rf .git` to remove the Git file from the repo and `git init` for a clean git history
+4. `npm install` to download dependancies
+5. In the terminal, enter `gulp watch` to make everything run
+6. Your web page should pop up at `http://localhost:3000`.
+7. Edit your Sass code inside of the scss folder.
+9. Keep gulp running while you're making changes. When you want to close out of the gulp task, in the terminal, hit `ctrl + C`.
+10. Happy coding.
---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+//gulpfile.js
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync').create(),
+    newer = require('gulp-newer'), 
+    notify  = require('gulp-notify'),
+    imagemin = require('gulp-imagemin');
+
+
+//style paths
+var sassFiles = 'styles/**/*.scss',
+    cssDest = 'styles/';
+
+/**
+ * Styles
+*/
+gulp.task('styles', function(){
+    gulp.src('styles/main.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(cssDest));
+});
+
+/**
+ * Images
+*/
+gulp.task('images', function() {
+
+// Add the newer pipe to pass through newer images only
+    return  gulp.src(['img/*.{png,jpg,gif}'])
+                .pipe(newer('img/*/'))
+                .pipe(imagemin({ optimizationLevel: 7, progressive: true, interlaced: true }))
+                .pipe(gulp.dest('img/'))
+                .pipe( notify( { message: 'Images task complete', onLast: true } ) );
+});
+gulp.task('watch',function() {
+	browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch('img/*.{png,jpg,gif}', ['images']).on('change', browserSync.reload);
+    gulp.watch(sassFiles,['styles']).on('change', browserSync.reload);
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+gulp.task('build', ['styles', 'images']);
+
